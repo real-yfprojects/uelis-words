30ad6a2b1d197968fb703221b2e6ee10c3d40fe0
MingXu <>
2023-11-14 12:31:06 +0100

    transcribe to own bash file

--- convert.sh
+++ convert.sh
@@ -9,6 +9,4 @@ mkdir -p outputs
 pip install git+https://github.com/openai/whisper.git
 
 # transcribe each .mp3 using whisper as vtt, about 10GB VRAM for large-v2 (how much ram?)
-for filename in videos/*.mp4; do
-    whisper "$filename" --model large-v2 --model_dir models/ --output_dir outputs/ --output_format vtt --language de
-done
+bash transcribe.sh
