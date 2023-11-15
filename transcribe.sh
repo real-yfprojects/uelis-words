dcd3b90af538fd346554fe0f8c3b872074d10c5d
MingXu <>
2023-11-15 17:09:57 +0100

    added ffmpeg

--- transcribe.sh
+++ transcribe.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -exu pipefail
 
+for filename in videos/*.mp4; do
+    ffmpeg -i "$filename" -acodec pcm_s16le -ac 1 -ar 16000 "wavs/$(basename "$filename" .mp4).wav"
+done
+
 # transcribe each .wav using whisper.cpp as json
 for filename in wavs/*.wav; do
     ./whisper.cpp/main -l de -m whisper.model --output-json-full -pp -of "outputs/$(basename "$filename" .wav)" -f "$filename"
