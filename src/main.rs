8be22425d90020ebe0da5943e67005dc33bf4c69
MingXu <>
2023-11-17 09:19:41 +0100

    split wavs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use serde::Deserialize;
 use std::env::var;
 use std::fs::File;
-use std::path::PathBuf;
-use std::process::exit;
+use std::path::{Path, PathBuf};
+use std::process::{exit, Command};
 
 use reqwest::blocking::Client;
 
@@ -63,6 +63,8 @@ fn main() -> anyhow::Result<()> {
         .filter(|l| !l.is_empty())
         .collect::<Vec<_>>();
 
+    std::fs::create_dir_all("wavs")?;
+
     for episode in metadata.episodes {
         let links: EpisodeMetadata = client
             .get(&format!(
@@ -92,7 +94,12 @@ fn main() -> anyhow::Result<()> {
         let mut filename = videos.join(&episode.created_at);
         filename.set_extension("mp4");
 
-        if transcribed.iter().all(|t| !t.contains(&episode.created_at)) {
+        if transcribed
+            .iter()
+            .filter(|t| t.contains(&episode.created_at))
+            .count()
+            != 2
+        {
             filename.set_extension("tmp");
 
             println!(
@@ -109,11 +116,116 @@ fn main() -> anyhow::Result<()> {
 
             std::fs::rename(&filename, filename.with_extension("mp4"))?;
             println!("Downloaded `{}`.", episode.id);
+
+            split_video(&filename.with_extension("mp4"))?;
         } else {
             println!("`{}` already exists.", episode.id);
         }
     }
     Ok(())
+}
+
+fn split_video(filename: &Path) -> anyhow::Result<()> {
+    // read `ffmpeg -i filename -af silencedetect=noise=-30dB:d=3.0 -f null - 2> out.txt` and try to split at exactly middle of video
+    let output = std::process::Command::new("ffmpeg")
+        .arg("-i")
+        .arg(filename)
+        .arg("-af")
+        .arg("silencedetect=noise=-30dB:d=1.0")
+        .arg("-f")
+        .arg("null")
+        .arg("-")
+        .output()?;
+    if !output.status.success() {
+        eprintln!("Failed to get video duration.");
+        eprintln!("Status code: {}", output.status);
+        eprintln!();
+        eprintln!("Response: {:?}", output);
+        exit(output.status.code().unwrap_or(1));
+    }
+    let output = String::from_utf8(output.stderr)?;
+    // format is `Duration: 00:00:00.00, start: 0.000000, bitrate: 0 kb/s`
+    let duration = output
+        .lines()
+        .find(|l| l.contains("Duration: ") && l.contains(", start: ") && l.contains(", bitrate: "))
+        .expect("Didn't find duration.")
+        .split_once("Duration: ")
+        .unwrap()
+        .1
+        .split_once(", start: ")
+        .unwrap()
+        .0
+        .split(':')
+        .map(|s| s.parse::<f64>().unwrap())
+        .reduce(|a, b| 60.0 * a + b)
+        .unwrap();
+    let output_lines = output
+        .lines()
+        .filter(|l| l.contains("silence_end"))
+        .collect::<Vec<_>>();
+    let middle = output_lines
+        .into_iter()
+        .map(|l| {
+            l.split_once("silence_end: ")
+                .unwrap()
+                .1
+                .split_once(" | silence_duration: ")
+                .unwrap()
+        })
+        .map(|(a, b)| (a.parse::<f64>().unwrap(), b.parse::<f64>().unwrap()))
+        .map(|(e, d)| (e - duration / 2.0, d))
+        .max_by_key(|(a, _)| a.trunc() as usize)
+        .map(|(e, d)| e - d / 2.0)
+        .unwrap();
+
+    println!("Splitting at `{middle}`.");
+
+    let output = PathBuf::from("wavs")
+        .join(filename.file_name().unwrap())
+        .with_extension("wav");
+
+    let p1 = split(filename)
+        .arg("-t")
+        .arg(format!("{}", middle.round() as usize))
+        .arg(output.with_extension("1.wav"))
+        .output()?;
+    if !p1.status.success() {
+        eprintln!("Failed to split video (1).");
+        eprintln!("Status code: {}", p1.status);
+        eprintln!();
+        eprintln!("Response: {:?}", p1);
+        exit(p1.status.code().unwrap_or(1));
+    }
+    println!("Saved part 1.");
+    let p2 = split(filename)
+        .arg("-ss")
+        .arg(format!("{}", middle.round() as usize))
+        .arg(output.with_extension("2.wav"))
+        .output()?;
+    if !p2.status.success() {
+        eprintln!("Failed to split video (2).");
+        eprintln!("Status code: {}", p2.status);
+        eprintln!();
+        eprintln!("Response: {:?}", p2);
+        exit(p2.status.code().unwrap_or(1));
+    }
+    println!("Saved part 2.");
+    Ok(())
+}
+
+fn split(filename: &Path) -> Command {
+    let mut command = Command::new("ffmpeg");
+    command
+        .arg("-i")
+        .arg(filename)
+        .arg("-acodec")
+        .arg("pcm_s16le")
+        .arg("-ac")
+        .arg("1")
+        .arg("-ar")
+        .arg("16000")
+        .arg("-y");
+    command
 }
 
 #[derive(Deserialize, Debug, Clone, PartialEq, Eq)]
--- transcribe.sh
+++ transcribe.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -exu pipefail
 
-for filename in videos/*.mp4; do
-    ffmpeg -i "$filename" -acodec pcm_s16le -ac 1 -ar 16000 "wavs/$(basename "$filename" .mp4).wav"
-done
-
 # transcribe each .wav using whisper.cpp as json
 for filename in wavs/*.wav; do
-    ./whisper.cpp/main -l de -m whisper.cpp/models/ggml-large-v2q.bin --output-json-full -pp -of "outputs/$(basename "$filename" .wav)" -f "$filename"
-    echo "$filename" >> transcribed.txt
-    break;
+    if grep -Fxq "$FILENAME" transcribed.txt
+    then
+        ./whisper.cpp/main -l de -m whisper.cpp/models/ggml-large-v2q.bin --output-json-full -pp -of "outputs/$(basename "$filename" .wav)" -f "$filename"
+        echo "$filename" >> transcribed.txt
+        break;
+    fi
 done
