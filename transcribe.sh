dc3fd92e35b2b1cc0c4664ecc1a0cf0d8ca3912b
MingXu <>
2023-11-17 09:40:43 +0100

    .

--- transcribe.sh
+++ transcribe.sh
@@ -3,7 +3,7 @@ set -exu pipefail
 
 # transcribe each .wav using whisper.cpp as json
 for filename in wavs/*.wav; do
-    if grep -Fxq "$FILENAME" transcribed.txt
+    if grep -Fxq "$filename" transcribed.txt
     then
         ./whisper.cpp/main -l de -m whisper.cpp/models/ggml-large-v2q.bin --output-json-full -pp -of "outputs/$(basename "$filename" .wav)" -f "$filename"
         echo "$filename" >> transcribed.txt
